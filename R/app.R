61adfe76b5b0ea024a0ffdd09f06ba513b582008
hettling <hannes.hettling@gmail.com>
2019-04-02 15:33:04 +0200

    points, panel

--- R/app.R
+++ R/app.R
@@ -5,29 +5,51 @@ library(leaflet)
 ##names(r_colors) <- colors()
 
 ui <- fluidPage(
-    ##    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("mymap"),
+    tags$style(type = "text/css", "html, body {width:100%;height:200%}"),
+    leafletOutput("mymap", height="95vh"),
+
+    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                  draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                  width = 330, height = "auto",
+                  
+                  h2("Text here"),
+                  
+                  ##selectInput("color", "Color", vars),
+                  ##selectInput("size", "Size", vars, selected = "adultpop"),
+                  conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                        # Only prompt for threshold when coloring or sizing by superzip
+                                   numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                   )), 
+
+
     p()##,
     ##  actionButton("recalc", "New points")
 )
 
 server <- function(input, output, session) {
 
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
-
   output$mymap <- renderLeaflet({
-      leaflet() %>%
-          addTiles() %>%  # Add default OpenStreetMap map tiles
-          addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
-      ##    leaflet() %>%
-      ##      addProviderTiles(providers$Stamen.TonerLite,
-      ##        options = providerTileOptions(noWrap = TRUE)
-      ##      ) %>%
-      ##      addMarkers(data = points())
+      leaflet() %>%          
+          addProviderTiles(providers$OpenStreetMap) %>%
+          addMarkers(tab$lon, tab$lat, popup=paste(tab$actuele_naam_oko, "<br>",
+                                                   tab$opvanglocatie_adres, "<br>",
+                                                   tab$opvanglocatie_postcode, " ",
+                                                   tab$opvanglocatie_woonplaats))
   })
 }
 
+if (FALSE) {
+tab <- read.csv('../data/export_opendata_lrk.csv', header=T, sep=";")
+tab <- tab[1:10,]
+
+data <- geocode_OSM(paste(as.character(tab$opvanglocatie_adres),
+                  as.character(tab$opvanglocatie_postcode),
+                  as.character(tab$opvanglocatie_woonplaats)))
+
+tab$lat <- data$lat
+tab$lon <- data$lon
+}
+
+    
 shinyApp(ui, server)
 
